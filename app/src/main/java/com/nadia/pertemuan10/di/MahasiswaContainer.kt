4796db8a9c083885389b49e4ac323f7f673b3cf9
veresoora <lahanabira@gmail.com>
2024-12-29 06:25:09 +0700

    menambahkan class MahasiswaContainer dengan retrofit dan json

--- app/src/main/java/com/nadia/pertemuan10/di/MahasiswaContainer.kt
+++ app/src/main/java/com/nadia/pertemuan10/di/MahasiswaContainer.kt
@@ -1,7 +1,30 @@
 package com.nadia.pertemuan10.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.nadia.pertemuan10.repository.MahasiswaRepository
+import com.nadia.pertemuan10.service_api.MahasiswaService
+import com.nadia.pertemuan10.service_api.NetworkKontrakRepository
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer{
+
+    private val baseUrl = "http://10.0.2.2:8000/UmyTI/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkKontrakRepository(mahasiswaService)
+    }
 }
