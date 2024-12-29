e5dfcff32276b13b18bf00e65d904c0e012bcbd1
veresoora <lahanabira@gmail.com>
2024-12-30 00:54:56 +0700

    Menambahkan fungsi OnLoading untuk tampilan loading

--- app/src/main/java/com/nadia/pertemuan10/ui/view/HomeView.kt
+++ app/src/main/java/com/nadia/pertemuan10/ui/view/HomeView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -34,6 +35,15 @@ import org.w3c.dom.Text
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Home Mhs"
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.iconsloading),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
 
 @Composable
