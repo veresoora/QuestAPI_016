fe68b523b80ee3ac33f219145559ccd0d065bc4e
veresoora <lahanabira@gmail.com>
2024-12-29 19:36:49 +0700

    Menambahkan class HomeViewModel, variabel mhsUiState, fungsi getMhs dan fungsi untuk delete

--- app/src/main/java/com/nadia/pertemuan10/viewmodel/HomViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan10/viewmodel/HomViewModel.kt
@@ -1,9 +1,52 @@
 package com.nadia.pertemuan10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.nadia.pertemuan10.model.Mahasiswa
+import com.nadia.pertemuan10.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error: HomeUiState()
     object Loading: HomeUiState()
+}
+
+class HomViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e:HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
