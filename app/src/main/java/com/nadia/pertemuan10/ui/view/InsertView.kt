e4314926215bad074578f2ad297e1583dca68203
veresoora <lahanabira@gmail.com>
2024-12-29 21:41:41 +0700

    Menambahkan object DestinasiEntry pada InsertView

---
+++ app/src/main/java/com/nadia/pertemuan10/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.nadia.pertemuan10.ui.view
+
+import com.nadia.pertemuan10.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry_Mhs"
+}
