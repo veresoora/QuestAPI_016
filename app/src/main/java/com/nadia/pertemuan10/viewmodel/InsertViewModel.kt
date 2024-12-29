8cff5b4c49c61e0e604f7f6bd033d74dee6576c1
veresoora <lahanabira@gmail.com>
2024-12-29 09:40:51 +0700

    Menambahkan fungsi InsertUiEvent.toMhs

--- app/src/main/java/com/nadia/pertemuan10/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan10/viewmodel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.nadia.pertemuan10.viewmodel
 
+import com.nadia.pertemuan10.model.Mahasiswa
+
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
@@ -11,4 +13,13 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
