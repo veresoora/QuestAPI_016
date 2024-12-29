d98bbfba6acefe54ffa87ae6c1a5e232097511f0
veresoora <lahanabira@gmail.com>
2024-12-30 01:45:59 +0700

    Mengubah Greeting menjadi MahasiswaApp

--- app/src/main/java/com/nadia/pertemuan10/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan10/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nadia.pertemuan10.ui.theme.Pertemuan10Theme
+import com.nadia.pertemuan10.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan10Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
