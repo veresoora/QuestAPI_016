9d8f574ccddf29bd838020bc2cd520b149705342
veresoora <lahanabira@gmail.com>
2024-12-29 09:34:31 +0700

    Menambahkan data class InsertUiEvent

---
+++ app/src/main/java/com/nadia/pertemuan10/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.nadia.pertemuan10.viewmodel
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
