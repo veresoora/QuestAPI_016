0a55f67044d3831653cccf54c331c6d4521df675
veresoora <lahanabira@gmail.com>
2024-12-29 09:50:09 +0700

    Menambahkan class InsertViewModel

--- app/src/main/java/com/nadia/pertemuan10/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan10/viewmodel/InsertViewModel.kt
@@ -1,6 +1,32 @@
 package com.nadia.pertemuan10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nadia.pertemuan10.model.Mahasiswa
+import com.nadia.pertemuan10.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = InsertUiEvent())
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
