1d9f5dcd224a455062c74a595eefe5d7fe1aa231
veresoora <lahanabira@gmail.com>
2024-12-29 23:21:18 +0700

    Menambahkan PenyediaViewModel

---
+++ app/src/main/java/com/nadia/pertemuan10/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.nadia.pertemuan10.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.nadia.pertemuan10.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository            )
+        }
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+    }
+}
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
