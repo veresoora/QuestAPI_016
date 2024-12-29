90f8457f12207c048494e09009761d906d7d1ca3
veresoora <lahanabira@gmail.com>
2024-12-29 23:46:17 +0700

    Menambahkan object DestinasiHome

---
+++ app/src/main/java/com/nadia/pertemuan10/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.nadia.pertemuan10.ui.view
+
+import com.nadia.pertemuan10.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
