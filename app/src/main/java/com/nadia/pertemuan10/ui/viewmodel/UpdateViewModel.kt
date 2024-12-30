ab8c6127427a763a0f4e8bc6acbf8eafd80ce45e
veresoora <lahanabira@gmail.com>
2024-12-30 12:40:57 +0700

    Menambahkan fungsi UpdateViewModel untuk melakukan update

---
+++ app/src/main/java/com/nadia/pertemuan10/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,38 @@
+package com.nadia.pertemuan10.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nadia.pertemuan10.repository.MahasiswaRepository
+import com.nadia.pertemuan10.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+class UpdateViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+): ViewModel(){
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswaNim(_nim)
+                .toUiStateMhs()
+        }
+    }
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun updateMhs(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
