0040e3897d0743841a58b69b4574269192859407
veresoora <lahanabira@gmail.com>
2024-12-30 12:23:54 +0700

    Menambahkan inisialisi DetailViewModel di PenyediaViewModel

--- app/src/main/java/com/nadia/pertemuan10/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan10/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.nadia.pertemuan10.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,6 +14,12 @@ object PenyediaViewModel {
         }
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiKontak().container.kontakRepository
+            )
         }
     }
 }
