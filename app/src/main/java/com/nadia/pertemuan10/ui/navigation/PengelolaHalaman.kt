de6cb5963d2a83327e22251585b2753814241a58
veresoora <lahanabira@gmail.com>
2024-12-30 12:29:35 +0700

    Menambahkan value untuk onDetailClick di HomeScreen dan navigasi untuk DetailScreen

--- app/src/main/java/com/nadia/pertemuan10/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/nadia/pertemuan10/ui/navigation/PengelolaHalaman.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.nadia.pertemuan10.ui.view.DestinasiDetail
 import com.nadia.pertemuan10.ui.view.DestinasiEntry
 import com.nadia.pertemuan10.ui.view.DestinasiHome
+import com.nadia.pertemuan10.ui.view.DetailScreen
 import com.nadia.pertemuan10.ui.view.EntryMhsScreen
 import com.nadia.pertemuan10.ui.view.HomeScreen
 
@@ -23,8 +27,10 @@ fun PengelolaHalaman(
     ){
         composable(DestinasiHome.route){
             HomeScreen(
-                navigateToltemEntry = { navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {
+                navigateToltemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println("PengelolaHalaman: nim = $nim")
                 }
             )
         }
@@ -38,6 +44,30 @@ fun PengelolaHalaman(
                     }
                 }
             )
+        }
+        composable (
+            DestinasiDetail.routeWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ){
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+
+            nim?.let { nim ->
+                DetailScreen(
+                    navigateBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    navigateToItemUpdate = {
+                    }
+                )
+            }
         }
     }
 }
