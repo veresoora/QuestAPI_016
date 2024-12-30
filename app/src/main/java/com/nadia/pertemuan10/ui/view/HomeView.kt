e12ef1ee8012d6360338ccfcb08f485b63396de5
veresoora <lahanabira@gmail.com>
2024-12-30 09:58:44 +0700

    Menambahkan fungsi KonfirmasiDeleteData untuk alert saat akan hapus data

--- app/src/main/java/com/nadia/pertemuan10/ui/view/HomeView.kt
+++ app/src/main/java/com/nadia/pertemuan10/ui/view/HomeView.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -27,8 +28,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -193,6 +199,9 @@ fun MhsCard(
     modifier: Modifier = Modifier,
     onDeleteClick: (Mahasiswa) -> Unit ={}
 ) {
+
+    var deleteConfirmation by rememberSaveable { mutableStateOf(false) }
+
     Card (
         modifier = modifier,
         shape = MaterialTheme.shapes.medium,
@@ -211,7 +220,7 @@ fun MhsCard(
                     style = MaterialTheme.typography.titleLarge
                 )
                 Spacer(Modifier.weight(1f))
-                IconButton(onClick = { onDeleteClick(mahasiswa) }) {
+                IconButton(onClick = {deleteConfirmation = true}) {
                     Icon(
                         imageVector = Icons.Default.Delete,
                         contentDescription = null,
@@ -232,4 +241,38 @@ fun MhsCard(
             )
         }
     }
+
+    if (deleteConfirmation) {
+        KonfirmasiDeleteData(
+            onDeleteConfirm = {
+                deleteConfirmation = false
+                onDeleteClick(mahasiswa)
+            },
+            onDeleteCancel =  {
+                deleteConfirmation = false
+            }, modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+@Composable
+private fun KonfirmasiDeleteData (
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = {},
+        title = { Text("Hapus Data") },
+        text = { Text("Apakah Anda benar-benar ingin menghapus data ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton (onClick = onDeleteCancel) {
+                Text(text = "Batal")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
 }
